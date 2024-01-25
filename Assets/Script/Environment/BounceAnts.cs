3aa74556cd5f924e96fe5909240e3202aa99fa5f
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2024-01-25 21:11:33 +0100

    fixed bounce

--- Assets/Script/Environment/BounceAnts.cs
+++ Assets/Script/Environment/BounceAnts.cs
@@ -11,15 +11,16 @@ public class BounceAnts : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
         {
-            // Calcola la direzione da cui il player sta arrivando rispetto all'oggetto
-            Vector2 direction = ((Vector2)collision.gameObject.transform.position - collision.GetContact(0).point).normalized;
-            Vector2 reflectedDir = Vector2.Reflect(-direction, Vector2.up);
+            //// Calcola la direzione da cui il player sta arrivando rispetto all'oggetto
+            //Vector2 direction = ((Vector2)collision.gameObject.transform.position - collision.GetContact(0).point).normalized;
+            //Vector2 reflectedDir = Vector2.Reflect(-direction, Vector2.up);
 
             // Applica una forza al rigidbody del player nella direzione calcolata
             Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
             if (playerRigidbody != null)
             {
-                playerRigidbody.AddForce(reflectedDir * bounceForce, ForceMode2D.Impulse);
+                Vector2 force = new Vector2(0f, bounceForce);
+                playerRigidbody.AddForce(force, ForceMode2D.Impulse);
             }
         }
     }
