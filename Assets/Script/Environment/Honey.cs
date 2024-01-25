0906cf1c720e22e65bbd54a229a0579f3b5f2515
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2024-01-25 13:36:40 +0100

    Initial adding of honey effect

--- Assets/Script/Environment/Honey.cs
+++ Assets/Script/Environment/Honey.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class Honey : EnvironmentEffects
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float attachmentAngle = 45f; // Angolazione di attaccamento al muro
+
+    [SerializeField]
+    private float attachmentCoefficient = 5f; // Coefficiente di attaccamento al muro
+
+    private bool isAttached = false;
+    private Vector2 wallNormal = Vector2.up; // Normale del muro (inizializzata a Vector2.up, ma dovresti impostarla in base alla configurazione del tuo gioco)
+
+    protected override void Update()
     {
-        
+        base.Update();
+
+        if (isAttached)
+        {
+            // Applica la logica di attaccamento
+            AttachToWall();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AttachToWall()
     {
-        
+        if (playerController != null)
+        {
+            // Verifica se il personaggio ha superato l'angolazione di attaccamento
+            float angle = Vector2.Angle(Vector2.up, wallNormal);
+
+            if (angle < attachmentAngle)
+            {
+                // Calcola la forza di attaccamento al muro
+                float attachmentForce = attachmentCoefficient * Mathf.Abs(playerController.gameObject.GetComponent<Rigidbody2D>().gravityScale);
+
+                // Applica la forza di attaccamento al muro
+                playerController.GetComponent<Rigidbody2D>().velocity = new Vector2(playerController.GetComponent<Rigidbody2D>().velocity.x, -attachmentForce);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playerController != null && !isAttached)
+        {
+            // Attiva l'attaccamento al muro quando il personaggio entra nella zona del miele
+            isAttached = true;
+
+            // Ottieni la normale del muro quando il personaggio entra nella zona del miele
+            CalculateWallNormal();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (playerController != null && isAttached)
+        {
+            // Disattiva l'attaccamento al muro quando il personaggio esce dalla zona del miele
+            isAttached = false;
+        }
+    }
+
+    private void CalculateWallNormal()
+    {
+        // Calcola la normale del muro (modifica questa logica in base alla configurazione del tuo gioco)
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, LayerMask.GetMask("Wall"));
+        if (hit.collider != null)
+        {
+            wallNormal = hit.normal;
+        }
     }
 }
