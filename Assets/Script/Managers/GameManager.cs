df5d065274a83ec033f216b99f20596409e5cc6b
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2024-01-28 02:15:44 +0100

    lvl management

--- Assets/Script/Managers/GameManager.cs
+++ Assets/Script/Managers/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
 {
     static public GameManager instance;
 
+    public Dictionary<int, bool> lvlStatus = new Dictionary<int, bool>();
+
     private void Awake()
     {
         if (instance == null)
@@ -18,6 +20,35 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+    public bool GetLevelStatus(int id)
+    {
+        foreach (KeyValuePair<int, bool> lvl in lvlStatus)
+        {
+            if (lvl.Key == id)
+            {
+                return lvl.Value;
+            }
+        }
+
+        return false;   
+    }
+
+    public void AddLevel(int lvlNum, bool status)
+    {
+        lvlStatus.Add(lvlNum, status);
+    }
+
+    public void EditLevel(int lvlNum, bool status)
+    {
+        foreach (KeyValuePair<int, bool> lvl in lvlStatus)
+        {
+            if (lvl.Key == lvlNum)
+            {
+                lvlStatus[lvlNum] = status;
+            }
+        }
+    }
+
     public void Exit()
     {
         Application.Quit();
@@ -26,6 +57,30 @@ public class GameManager : MonoBehaviour
     public void LoadNextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadLevel(int id)
+    {
+        // Verifica se la chiave id esiste nella mappa dei livelli
+        if (lvlStatus.ContainsKey(id))
+        {
+            // Verifica se il livello è stato sbloccato
+            if (lvlStatus[id])
+            {
+                // Carica il livello corrispondente alla chiave id
+                SceneManager.LoadScene(id);
+            }
+            else
+            {
+                // Il livello non è stato sbloccato, quindi non può essere caricato
+                Debug.LogWarning("Il livello " + id + " non è stato sbloccato.");
+            }
+        }
+        else
+        {
+            // La chiave id non esiste nella mappa dei livelli
+            Debug.LogError("La chiave " + id + " non esiste nella mappa dei livelli.");
+        }
     }
 
     private void Update()
